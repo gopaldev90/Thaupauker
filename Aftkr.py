8f82d74bec3cd2be5f5d283c384117cc8433687b
Gopal Krishna Pandey <gopaldev9076@gmail.com>
2024-10-10 10:01:21 +0530

    Add files via upload

--- Aftkr.py
+++ Aftkr.py
@@ -1,7 +1,15 @@
 import os
 import copy
 import json
+import platform
 from datetime import datetime
+import sys
+def get_data_path(filename):
+    if getattr(sys, 'frozen', False):  # Check if the app is frozen (i.e., bundled)
+        return os.path.join(sys._MEIPASS, filename)  # _MEIPASS is where PyInstaller stores the bundled files
+    else:
+        return filename  # Otherwise, use the normal filename
+
 
 class PVZ2Modifier:
     '''
@@ -11,14 +19,61 @@ class PVZ2Modifier:
     '''
     
     def __init__(self):
+        if platform.system()=='Linux':
+        	import sys
+        	sys.path.append('/storage/emulated/0/pvz2/plant 1000 bnao/Tool/')
+        	
+        	self.__alreadypath='/storage/emulated/0/pvz2/plant 1000 bnao/Tool/alrdy.json'
+        else:
+        	self.__alreadypath=get_data_path('alrdy.json')
         self.logs=""
-        self.__eligipau = [
-            7, 15, 27, 30, 35, 37, 39, 42, 45, 47, 49, 50, 52, 56
+        # yaad rhe ye sorted rhe(ascending)
+        
+        self.sphlist = [
+            4,
+            25,
+            41,
+            51,
+            59,
+            77,
+            102,
+            147 
         ]
-
+        self.__alrdymod= self.__loadjson(self.__alreadypath) 
+    def __loadjson(self, path):
+        if not os.path.exists(path):
+        	alson={
+        	'already': []
+        	}
+        else:
+        	with open(path, 'r') as f:
+        		alson= json.load(f)
+        return alson
     def getcurtime(self):
         return datetime.now().strftime("%d-%m-%Y %H:%M:%S:%f")[:-3]
 
+    def __geteligi(self,taton, list):
+        n = len(list)
+        step= int(n**0.5)
+        prev=curr = 0
+        while curr < n and list[curr] <= taton:
+            prev = curr
+            curr+=step
+            if curr>=n:
+                curr=n
+        
+        left, right =prev, curr-1
+        r = False
+        while (left<=right):
+            mid = (left + right) // 2
+            if(list[mid]==taton):
+                r=True
+                break
+            elif(list[mid]<taton):
+                left = mid+1
+            else:
+                right = mid-1
+        return r
     def __updatestatus(self, label, root, taton, Seema):
         '''
         Update the status label in the GUI.
@@ -34,7 +89,7 @@ class PVZ2Modifier:
         label.config(text=status_text)
         root.update_idletasks()
 
-    def savejson(self, saveto, paun_folder, data):
+    def savejson(self, saveto, paun_folder, data, ston=False):
         '''
         Save JSON data to a specified folder.
 
@@ -50,6 +105,12 @@ class PVZ2Modifier:
         with open(filep, 'w') as f:
             json.dump(data, f, indent=2)
         lop = os.path.join(fo, "log.txt")
+        if ston:
+        	import jsortokhi
+        	rton = jsortokhi.torton(filep)
+        	with open(os.path.join(fo,'PlantLevels.rton'),'wb')as f:
+        		f.write(rton)
+        	self.logs+='Rton converted'
         with open(lop, 'w') as f:
             f.write(self.logs)
         self.logs=""
@@ -101,7 +162,46 @@ class PVZ2Modifier:
                 if cleaned_alias == name.capitalize().translate(str.maketrans('', '', "/\\:*?\"<>|")):
                     return k  
         return -1
-
+    def groupupdate(self, data, cpf, tatonlist):
+        vyakhya= []
+        for taton in tatonlist:
+            paunaam= self.naamde(data, taton)
+            data=self.update_json(taton, data, cpf, paunaam)
+            
+            vyakhya.append(f"{paunaam} id: {taton}")
+        data["#vyakhya"]=vyakhya
+        data["#credit"]="Hacked by Pvz2pagalpan"
+        return data
+    def zeromana(self, data, label=False, root=False):
+        '''
+        changes every plant's recharge to 0.
+        Parameters:
+        data (dict): JSON data
+        label: tk.Label to update (optional)
+        root: tk.Tk root (optional)
+        return: (dict) Json data 
+        
+        '''
+        seema = self.getSeema(data)
+        for taton in range(seema + 1):
+            ch=0
+            paunaam= self.naamde(data, taton)
+            float_stats =data['objects'][taton]['objdata']['FloatStats']
+            for stat in float_stats:
+                name = stat.get('Name', '').lower()
+                if(any(substring in name for substring in ["cooldown","cost"])):
+                    if(name=="cost"):
+                    	stat["Values"] = [-2048] * len(stat["Values"])
+                    else:
+                    	stat["Values"] = [0] * len(stat["Values"])
+                    ch+=1
+                    if(ch>2):
+                    	break
+                    self.logs+=f"Zeroed {paunaam}'s {name}\n"
+            if label and root:
+                self.__updatestatus(label, root, taton, seema) 
+        return data
+    
     def sabalaga(self, data, saveto, label=False, root=False):
         '''
         Modify all plants and save the JSON files separately.
@@ -122,12 +222,12 @@ class PVZ2Modifier:
         for taton in range(Seema + 1):
             paun_folder = paunlist[taton]
             data = copy.deepcopy(shudhdata)
-            cpf = taton in self.__eligipau
-            data = self.update_json(taton, data, cpf, paun_folder)
+            
+            data = self.update_json(taton, data, False, paun_folder)
             vyakhya = f"{paun_folder} Hacked by Pvz2pagalpan Id: {taton}"
             data["#vyakhya"] = vyakhya
             data["#Kab"] = self.getcurtime()
-            self.savejson(saveto, (str(taton) + "." + paun_folder), data)
+            self.savejson(saveto, (str(taton) + "." + paun_folder), data, True)
             if label and root:
                 self.__updatestatus(label, root, taton, Seema)
 
@@ -143,9 +243,9 @@ class PVZ2Modifier:
         '''
         Seema = self.getSeema(data)
         for taton in range(Seema + 1):
-            cpf = taton in self.__eligipau
+            
             paunaam = self.naamde(data, taton)
-            data = self.update_json(taton, data, cpf, paunaam)
+            data = self.update_json(taton, data, False, paunaam)
             if label and root:
                 self.__updatestatus(label, root, taton, Seema)
         
@@ -153,9 +253,9 @@ class PVZ2Modifier:
         vyakhya = "All plants hacked by Pvz2pagalpan"
         data["#vyakhya"] = vyakhya
         data["#Kab"] = self.getcurtime()
-        self.savejson(saveto, paun_folder, data)
+        self.savejson(saveto, paun_folder, data, True)
 
-    def update_json(self, taton, data, cpf, paunam):
+    def update_json(self, taton, data, cpf, paunam, ignoreallr= False):
         '''
         Update the plant levels.
 
@@ -164,38 +264,44 @@ class PVZ2Modifier:
         data (dict): JSON data.
         paunam (str): Name of the plant.
         cpf (bool): Indicates PlantFood play count is to be updated.
-
+        ignoreallr (bool): ignore allready hacked stats if True it will hack again
         Returns:
         dict: Updated JSON data.
         '''
+        locallogs=""
+        if(not ignoreallr):
+        	alreadylist = self.__alrdymod['already']
+        	if(self.__geteligi(taton, alreadylist)):
+        		data['objects'][taton]= self.__alrdymod['plants'][str(taton)]['value']
+        		self.logs+=f'\n{paunam} already hacked\n'
+        		return data
         nodename = 'PlantFoodPlayCount'
         skiplist = [nodename.lower(), 'planttier', 'plantfoodduration', "knockback"]
 
         def checkodr(stat, name, gap):
             for ele in skiplist:
                 if not ele in name:
-                    asce = True
-                    desce = True
+                    asce = desce = True
                     originallist = statval[0:gap+1]
                     for k in range(1, len(originallist)):
                         if originallist[k] > originallist[k-1]:
                             desce = False
                         elif originallist[k] < originallist[k-1]:
                             asce = False
-                        if not asce and not desce:
+                        if(not(asce or desce)):
                             break
                     if asce or desce:
                         if asce:
                             statval[-1] = 90745545
                         else:
-                            statval[-1] = 0
+                            statval[-1] = 0.1
                         stat["Values"] = statval
                     break
             return stat
 
         maxlvl = 1000
         levelcoins_values = [0] * maxlvl
-        self.logs += f"{paunam} changes:\n"
+        locallogs += f"\n{paunam} changes:\n"
         float_stats = data['objects'][taton]['objdata']['FloatStats']
         conditionvalues = {
             "damage": 22339,
@@ -205,7 +311,8 @@ class PVZ2Modifier:
             "packetcooldown": 0,
             "startingcooldown": 0,
             "expirationduration": 12999990,
-            "velocity": 1124
+            "velocity": 1124,
+            "sunproduce": 907
         }
 
         for stat in float_stats:
@@ -225,16 +332,21 @@ class PVZ2Modifier:
                     break
             if ntco:
                 stat = checkodr(stat, name, gap)
-                self.logs+= 'used checkodr for '
-            self.logs += f'{name} last value\n{statval[-1]}\n'
-            print(self.logs)
-            if 'nut' in paunam and name == 'hitpoints':
-                stat["Values"][-1] = 21474836479999999
-
-        self.logs+= f"cpf was\n{cpf}\n"
+                locallogs+= 'used checkodr for '
+            locallogs += f'{name} last value\n{statval[-1]}\n'
+        # new nut logic here
+        if(self.__geteligi(taton, self.sphlist)):
+            name = 'hitpoints'
+            sh = 21474836479999999
+            index = next((i for i, stat in enumerate(float_stats) if stat["Name"].lower()== name), None)
+            if index is not None:
+                float_stats[index]["Values"][-1] = sh
+                locallogs = locallogs.replace(f"{name} last value\n99999999", f"{name} last value\n{sh}")
+            
+        locallogs+= f"cpf was\n{cpf}\n"
         if cpf:
             iseligible = not any(skiplist[2] in stat.get('Name', '').lower() for stat in float_stats)
-            self.logs+= f'Eligible\n{iseligible}\n'
+            locallogs+= f'Eligible\n{iseligible}\n'
             if iseligible:
                 
                 pfvalues = [1] * maxlvl
@@ -245,7 +357,7 @@ class PVZ2Modifier:
                         pfst = stat
                         break
                 if pfst:
-                    self.logs+=f'{nodename} was already in float_stats\n'
+                    locallogs+=f'{nodename} was already in float_stats\n'
                     pfst["Values"] = pfvalues
                 else:
                     pfpc_dict = {
@@ -253,17 +365,21 @@ class PVZ2Modifier:
                         'Values': pfvalues
                     }
                     float_stats.append(pfpc_dict)
-                    self.logs+=f'{nodename} added in float_stats\n'
+                    locallogs+=f'{nodename} added in float_stats\n'
             else:
                 pass
 
         data['objects'][taton]['objdata']['LevelCoins'] = levelcoins_values
-        self.logs+="LevelCoins changed\n"
+        locallogs+="LevelCoins changed\n"
         data['objects'][taton]['objdata']['LevelXP'] = levelcoins_values
-        self.logs+="LevelXP changed\n"
+        locallogs+="LevelXP changed\n"
         data['objects'][taton]['objdata']['LevelCap'] = maxlvl
-        self.logs += 'LevelCap changed\n'
+        locallogs += 'LevelCap changed\n'
         
+        
+        
+        
+        self.logs+=locallogs
         return data
 
 
--- Thaupauker.py
+++ Thaupauker.py
@@ -4,150 +4,195 @@ import copy
 import json
 import os, time, platform
 
-if platform.system()=='Linux':
+if platform.system() == 'Linux':
     import sys
     sys.path.append('/storage/emulated/0/pvz2/plant 1000 bnao/Tool/')
     
 import Aftkr
 
-root = tk.Tk()
-
-# Create an instance of the PVZ2Modifier class
-modifier = Aftkr.PVZ2Modifier()
-
-def startfindnam(data):
-    name = Tatonbox.get()
-    tmp = modifier.gettaton(data, name)
-    if tmp >= 0:
-        Tatonbox.delete(0, tk.END)
-        Tatonbox.insert(tk.END, tmp)
-        khojbtn.config(text="Khoj")
-        khojklk()
-    else:
-        Staus.config(text="Aisa koi paudha nhin h")
-
-def sabalag(pragati, root):
-    st = time.time()
-    sablgbtn.config(state=tk.DISABLED)
-    sabkmbtn.config(state=tk.DISABLED)
-    global data
-    global saveto
-    global file_path
-    st = time.time()
-    modifier.sabalaga(data, saveto, pragati, root)
-    setdata(file_path)
-    ed = time.time()
-    lga = ed - st
-    Staus.config(text=f"samay lga sab \n alag karne main seconds: {lga}")
-
-def khojklk():
-    global data, taton, paun_folder, saveto, oldtaton, file_path
-    Seema = modifier.getSeema(data)
-    try:
-        if khojbtn.cget("text") == "Confirm":
-            st = time.time()
-            paun_folder = Staus.cget('text')
-            data = modifier.update_json(taton, data, checkbox_var.get(), paun_folder)
-            data["#vyakhya"] = f"{paun_folder} Hacked by Pvz2pagalpan Id: {taton}"
-            data["#Kab"]= modifier.getcurtime()
-            
-            modifier.savejson(saveto, f"{str(taton)}.{paun_folder}", data)
-            lga = time.time() - st
-            setdata(file_path)
-            Staus.config(text=f"{paun_folder} Successfully \n hacked in {lga} seconds")
-            sabkmbtn.config(state=tk.DISABLED)
-            sablgbtn.config(state=tk.DISABLED)
-            khojbtn.config(text="Khoj")
-        else:
-            taton = Tatonbox.get()
-            if not taton.isdigit():
-                startfindnam(data)
-            else:
-                taton = int(taton)
-                if not (0 <= taton <= Seema):
-                    raise ValueError(f"Seema se paar. Seema: {Seema}")
-                else:
-                    oldtaton = taton
-                    Staus.config(text=modifier.naamde(data, taton))
-                    khojbtn.config(text="Confirm")
-                    checkbox.config(state=tk.NORMAL)
-    except Exception as e:
-        #raise ValueError(e)
-        Staus.config(text=f"Gadbad hai: {e}")
-
-def ekmsb(pragati, root):
-    sabkmbtn.config(state=tk.DISABLED)
-    sablgbtn.config(state=tk.DISABLED)
-    global data, saveto, file_path
-    st = time.time()
-    modifier.ekamsab(data, saveto, pragati, root)
-    setdata(file_path)
-    endm = time.time()
-    lga = endm - st
-    Staus.config(text=f"samay lga sab \n ek karne main seconds: {lga}")
-
-def open_file_dialog():
-    global file_path
-    file_path = filedialog.askopenfilename(
-        title="Select a JSON file",
-        filetypes=[("JSON files", "*.json"), ("All files", "*.*")]
-    )
-
-    if file_path:
-        global saveto
-        saveto = os.path.dirname(file_path) + "/Edited/"
-        sabkmbtn.config(state=tk.NORMAL)
-        khojbtn.config(state=tk.NORMAL)
-        sablgbtn.config(state=tk.NORMAL)
-        setdata(file_path)
-      
-def setdata(file_path):
-    global data
-    with open(file_path, 'r') as file:
-        ndata = json.load(file)
-    data = copy.deepcopy(ndata)
+class PVZ2App:
+    def __init__(self, root):
+        self.root = root
+        self.phone = platform.system() == 'Linux'
+        self.modifier = Aftkr.PVZ2Modifier()
+        self.file_path = self.data = self.saveto = self.paun_folder = self.oldtaton = self.taton = None
+        self.initisetxt()
+        # Initialize the GUI
+        self.init_gui()
+        
+    def initisetxt(self):
+    	self.khojtxt = "Khoj"
+    	self.confirmtxt = "Pushti krain"
+    	self.grouptxt = "Grout update"
     
-def textbdla(event):
-    try:
-        global oldtaton
-        taton = int(Tatonbox.get())
-        if taton != oldtaton:
-            khojbtn.config(text="Khoj")
+    
+    
+    def init_gui(self):
+        self.pragati = tk.Label(self.root, text="", fg="green")
+        self.pragati.place(relx=0.5, rely=0.58, anchor=tk.CENTER)
+
+        button = tk.Button(self.root, text="Select JSON File", fg='white', bg='green', command=self.open_file_dialog)
+        button.pack(pady=20)
+
+        self.sablgbtn = tk.Button(self.root, text="Sab Alag", command=self.sabalag)
+        self.sablgbtn.config(state=tk.DISABLED)
+        self.sablgbtn.place(relx=0.20, rely=0.18, anchor=tk.CENTER)
+
+        self.sabkmbtn = tk.Button(self.root, text="Sab Ekam", command=self.ekmsb)
+        self.sabkmbtn.config(state=tk.DISABLED)
+        self.sabkmbtn.place(relx=0.5, rely=0.29, anchor=tk.CENTER)
+
+        self.rerechbtn = tk.Button(self.root, text="0 Cooldown", command=self.zeromana)
+        self.rerechbtn.config(state=tk.DISABLED)
+        self.rerechbtn.place(relx=0.5, rely=0.38, anchor=tk.CENTER)
+
+        self.khojbtn = tk.Button(self.root, text=self.khojtxt, command=self.khojklk)
+        self.khojbtn.config(state=tk.DISABLED)
+        self.khojbtn.place(relx=0.73, rely=0.18, anchor=tk.CENTER)
+
+        self.Tatonbox = tk.Entry(self.root)
+        self.Tatonbox.pack()
+        self.Tatonbox.bind("<KeyRelease>", self.textbdla)
+
+        self.checkbox_var = tk.BooleanVar()
+        self.checkbox = tk.Checkbutton(self.root, text="Change PFPC", variable=self.checkbox_var)
+        self.checkbox.place(relx=0.5, rely=0.69, anchor=tk.CENTER)
+        self.checkbox.config(state=tk.DISABLED)
+
+        self.Staus = tk.Label(self.root, text="")
+        self.Staus.place(relx=0.5, rely=0.49, anchor=tk.CENTER)
+        
+        self.stm = tk.Label(self.root, text='Made in Bharat')
+        self.stm.place(relx=0.5, rely=0.98, anchor=tk.CENTER)
+        
+        self.root.resizable(False, False)
+        self.root.geometry("400x400")
+        
+        if self.phone:
+            self.open_file_dialog(True)
+    
+    def open_file_dialog(self,auto=False):
+        if ((not self.phone)or(not auto)):
+            self.file_path = filedialog.askopenfilename(
+                title="Select a JSON file",
+                filetypes=[("JSON files", "*.json"), ("All files", "*.*")]
+            )
         else:
-            khojbtn.config(text="Confirm")
-    except Exception as e:
-        khojbtn.config(text="Khoj")
+            self.file_path = '/storage/emulated/0/pvz2/plant 1000 bnao/Tool/PlantLevels.json'
+        
+        if self.file_path:
+            self.saveto = os.path.dirname(self.file_path) + "/Edited/"
+            self.sabkmbtn.config(state=tk.NORMAL)
+            self.khojbtn.config(state=tk.NORMAL)
+            self.sablgbtn.config(state=tk.NORMAL)
+            self.rerechbtn.config(state=tk.NORMAL)
+            self.setdata()
+    
+    def setdata(self):
+        with open(self.file_path, 'r') as file:
+            ndata = json.load(file)
+        self.data = copy.deepcopy(ndata)
+    
+    def startfindnam(self, name):
+        tmp = self.modifier.gettaton(self.data, name)
+        if tmp >= 0:
+            self.Tatonbox.delete(0, tk.END)
+            self.Tatonbox.insert(tk.END, tmp)
+            self.khojbtn.config(text=self.khojtxt)
+            self.khojklk()
+        else:
+            self.Staus.config(text="Aisa koi paudha nhin h")
+    
+    def zeromana(self):
+        st = time.time()
+        self.sablgbtn.config(state=tk.DISABLED)
+        self.sabkmbtn.config(state=tk.DISABLED)
+        self.rerechbtn.config(state=tk.DISABLED)
+        self.data = self.modifier.zeromana(self.data, self.pragati, self.root)
+        self.modifier.savejson(self.saveto, "0 Cooldown", self.data, True)
+        self.setdata()
+        ed = time.time()
+        lga = ed - st
+        self.Staus.config(text=f"samay lga zero mana\n karne main seconds: {lga}")
 
-pragati = tk.Label(root, text="", fg="green")
-pragati.place(relx=0.5, rely=0.56, anchor=tk.CENTER)
+    def sabalag(self):
+        st = time.time()
+        self.sablgbtn.config(state=tk.DISABLED)
+        self.sabkmbtn.config(state=tk.DISABLED)
+        self.modifier.sabalaga(self.data, self.saveto, self.pragati, self.root)
+        self.setdata()
+        ed = time.time()
+        lga = ed - st
+        self.Staus.config(text=f"samay lga sab \n alag karne main seconds: {lga}")
 
-button = tk.Button(root, text="Select JSON File", command=open_file_dialog)
-button.pack(pady=20)
+    def ekmsb(self):
+        self.sabkmbtn.config(state=tk.DISABLED)
+        self.sablgbtn.config(state=tk.DISABLED)
+        st = time.time()
+        self.modifier.ekamsab(self.data, self.saveto, self.pragati, self.root)
+        self.setdata()
+        endm = time.time()
+        lga = endm - st
+        self.Staus.config(text=f"samay lga sab \n ek karne main seconds: {lga}")
 
-sablgbtn = tk.Button(root, text="Sab Alag", command=lambda: sabalag(pragati, root))
-sablgbtn.config(state=tk.DISABLED)
-sablgbtn.place(relx=0.20, rely=0.18, anchor=tk.CENTER)
+    def khojklk(self):
+        Seema = self.modifier.getSeema(self.data)
+        try:
+            if self.khojbtn.cget("text") == self.confirmtxt:
+                st = time.time()
+                self.paun_folder = self.Staus.cget('text')
+                if self.paun_folder == self.grouptxt:
+                    sf = "group"
+                    self.data = self.modifier.groupupdate(self.data, self.checkbox_var.get(), self.taton)
+                else:
+                    sf = f"{str(self.taton)}.{self.paun_folder}"
+                    self.data = self.modifier.update_json(self.taton, self.data, self.checkbox_var.get(), self.paun_folder)
+                    self.data["#vyakhya"] = f"{self.paun_folder} Hacked by Pvz2pagalpan Id: {self.taton}"
+                self.data["#Kab"] = self.modifier.getcurtime()
+                self.modifier.savejson(self.saveto, sf, self.data, True)
+                lga = time.time() - st
+                self.setdata()
+                self.Staus.config(text=f"{self.paun_folder} Successfully \n hacked in {lga} seconds")
+                self.sabkmbtn.config(state=tk.DISABLED)
+                self.sablgbtn.config(state=tk.DISABLED)
+                self.khojbtn.config(text=self.khojtxt)
+            else:
+                self.taton = self.Tatonbox.get()
+                if not self.taton.isdigit() and "," not in self.taton:
+                    self.startfindnam(self.taton)
+                elif "," in self.taton:
+                    self.taton = [int(i) for i in self.taton.split(',')]
+                    if any(not (0 <= i <= Seema) for i in self.taton):
+                        raise ValueError(f"Koi number seema se paar Seema: {Seema}")
+                    self.Staus.config(text=self.grouptxt)
+                    self.khojbtn.config(text=self.confirmtxt)
+                    self.checkbox.config(state=tk.NORMAL)
+                else:
+                    self.taton = int(self.taton)
+                    if not (0 <= self.taton <= Seema):
+                        raise ValueError(f"Seema se paar. Seema: {Seema}")
+                    else:
+                        self.oldtaton = self.taton
+                        self.Staus.config(text=self.modifier.naamde(self.data, self.taton))
+                        self.khojbtn.config(text=self.confirmtxt)
+                        self.checkbox.config(state=tk.NORMAL)
+        except Exception as e:
+            v=0
+            if v:
+            	raise Exception(e)
+            self.Staus.config(text=f"Gadbad hai: {e}",fg='red')
 
-sabkmbtn = tk.Button(root, text="Sab Ekam", command=lambda: ekmsb(pragati, root))
-sabkmbtn.config(state=tk.DISABLED)
-sabkmbtn.place(relx=0.5, rely=0.29, anchor=tk.CENTER)
+    def textbdla(self, event):
+        try:
+            taton = int(self.Tatonbox.get())
+            self.khojbtn.config(text=self.khojtxt)
+            self.khojklk()
+            
+        except ValueError:
+            self.khojbtn.config(text=self.khojtxt)
 
-khojbtn = tk.Button(root, text="Khoj", command=khojklk)
-khojbtn.config(state=tk.DISABLED)
-khojbtn.place(relx=0.73, rely=0.18, anchor=tk.CENTER)
-
-Tatonbox = tk.Entry(root)
-Tatonbox.pack()
-Tatonbox.bind("<KeyRelease>", textbdla)
-
-checkbox_var = tk.BooleanVar()
-checkbox = tk.Checkbutton(root, text="Change PFPC", variable=checkbox_var)
-checkbox.place(relx=0.5, rely=0.63, anchor=tk.CENTER)
-checkbox.config(state=tk.DISABLED)
-
-Staus = tk.Label(root, text="")
-Staus.place(relx=0.5, rely=0.4, anchor=tk.CENTER)
-
-root.resizable(False, False)
-root.geometry("400x400")
-root.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = PVZ2App(root)
+    root.mainloop()
+    
